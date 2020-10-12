11a066373e2493e75ae6a026b28e8fc921c8eb89
Der_Joscha <d3r.j05ch4@gmail.com>
2020-10-12 20:54:51 +0200

    Add premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "stb"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"stb.c",
+		"stb_vorbis.c",
+
+		"stb.h",
+		"stb_c_lexer.h",
+		"stb_connected_components.h",
+		"stb_divide.h",
+		"stb_ds.h",
+		"stb_dxt.h",
+		"stb_easy_font.h",
+		"stb_herringbone_wang_tile.h",
+		"stb_image.h",
+		"stb_image_resize.h",
+		"stb_image_write.h",
+		"stb_include.h",
+		"stb_leakcheck.h",
+		"stb_perlin.h",
+		"stb_rect_pack.h",
+		"stb_sprintf.h",
+		"stb_textedit.h",
+		"stb_tilemap_editor.h",
+		"stb_truetype.h",
+		"stb_voxel_render.h",
+		"stretchy_buffer.h",
+	}
+
+	filter "configurations:Debug"
+	
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+	
+		runtime "Release"
+		optimize "on"
